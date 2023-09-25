ed66d9b0958202c6384d6de5baea43583ceed574
mahdi mehrabi <mahdimehrabi17@gmail.com>
2023-09-25 17:56:03 +0330

    Feat:Add user entity

--- src/entities/user.go
+++ src/entities/user.go
@@ -1,7 +1,9 @@
 package entities
 
 type User struct {
-	id       int64
-	email    string
-	fullname string
+	ID        int64
+	email     string
+	fullname  string
+	lastLogin int64    //this field is timestamp and must fill using authorization service and kafka
+	lastIPs   []string //user's 10 last logged in ip addresses and must fill using authorization service and kafka
 }
