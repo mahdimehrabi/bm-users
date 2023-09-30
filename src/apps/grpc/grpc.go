0e53568430660809f663aac99cc5cf609cfdd13d
mahdi mehrabi <mahdimehrabi17@gmail.com>
2023-09-30 16:23:03 +0330

    feat: reflection

--- src/apps/grpc/grpc.go
+++ src/apps/grpc/grpc.go
@@ -3,7 +3,9 @@ package grpc
 import (
 	"bm-users/src/apps/grpc/proto/user"
 	"bm-users/src/apps/grpc/servers"
+	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"net"
 )
 
@@ -16,7 +18,10 @@ func RunServer() {
 	var opts []grpc.ServerOption
 
 	grpcServer := grpc.NewServer(opts...)
-	user.RegisterUserServer(grpcServer, servers.UserServer{})
+	user.RegisterUserServer(grpcServer, &servers.UserServer{})
+	reflection.Register(grpcServer)
+
+	fmt.Println("runnnn")
 	err = grpcServer.Serve(l)
 	if err != nil {
 		panic(err)
